0ae5ac351785b17e5419040324f4e76011c07b75
kevinchewning <klcbusiness@hotmail.com>
2021-08-15 13:57:30 -0400

    Setup files and directories

---
+++ db/schema.sql
@@ -0,0 +1,41 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+-- Creates the "employee_tracker_db" database --
+CREATE DATABASE employee_tracker_db;
+
+-- Makes it so all of the following code will affect employee_tracker_db --
+USE employee_tracker_db;
+
+-- Creates the table "department" within employee_tracker_db --
+CREATE TABLE department (
+  id INT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+-- Creates the table "role" within employee_tracker_db --
+CREATE TABLE role (
+  id INT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
+-- Creates the table "employee" within employee_tracker_db --
+CREATE TABLE employee (
+  id INT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL,
+  manager_id INT,
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
+);
+
+
